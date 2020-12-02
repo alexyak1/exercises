db524a8d7bdad9553f4aca4907c34da4f5015d4d
alexyak1 <yakimchuk.nstu@gmail.com>
2020-12-02 14:05:09 +0100

    clean up a bit first task

--- andventCode/1th.py
+++ andventCode/1th.py
@@ -1,19 +1,16 @@
+numbers = []
+
 with open("input.txt") as f:
     lines = f.readlines()
-
-numbers = []
 
 for line in lines:
     numbers.append(line.rstrip())
 
 
-firstVal = 0
-
 for i in range(0, len(numbers)):
-    firstVal = numbers[i]
     for j in range(0, len(numbers)):
-        sum = int(firstVal) + int(numbers[j])
+        sum = int(numbers[i]) + int(numbers[j])
         if sum == 2020:
-            result = int(firstVal) * int(numbers[j])
+            result = int(numbers[i]) * int(numbers[j])
 
 print("Done! You number is ", result)
---
+++ andventCode/README.md
@@ -0,0 +1,4 @@
+# This is solutions from Adventofcode 2020
+
+- 1th tash: Find the two entries that sum to 2020; what do you get if you multiply them together?
+
